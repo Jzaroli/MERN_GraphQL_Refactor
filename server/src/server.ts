76c00890fe1453482c52cc737e34e66adecbb8d7
Johann Zaroli <175734270+Jzaroli@users.noreply.github.com>
2025-01-27 19:34:25 -0700

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,12 +7,17 @@ import { authenticateToken } from './services/auth.js';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 
+import { fileURLToPath } from 'url';
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const startApolloServer = async () => {
   await server.start();
